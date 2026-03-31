e8fa535db1dee360ace348bff645be948987dbae
franciscoabsampaio <franciscoabsampaio@proton.me>
2026-03-31 21:32:28 +0100

    Fix: test

--- src/dorans/death.py
+++ src/dorans/death.py
@@ -31,7 +31,10 @@ def time_increase_factor(game_minutes: int | float) -> int | float:
     Returns:
         float: The time increase factor.
     """
-    game_minutes += 0.5  # Adjust for documented values
+    if game_minutes < 0:
+        raise ValueError("Game time cannot be negative.")
+    
+    game_minutes += 0.5  # For some reason documented values are shifted by 0.5
 
     if game_minutes < 15:
         return 0
@@ -67,7 +70,5 @@ def timer(
     """
     if level < 1 or level > 18:
         raise ValueError("Level must be between 1 and 18.")
-    if game_minutes < 0:
-        raise ValueError("Game time cannot be negative.")
     
     return BASE_RESPAWN_WAIT_TIME_PER_LEVEL[level] * (1 + time_increase_factor(game_minutes))
